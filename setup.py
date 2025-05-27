8f4b7e40b389871cfbae78ebb77edec5d53d09ed
John Schember <john@nachtimwald.com>
2025-05-27 19:31:19 -0400

    Include all files in sdist. Compile shaders for whl.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+from itertools import chain
+from pathlib import Path
+
+from setuptools import setup
+from setuptools.command.build_py import build_py as _build_py
+
+class build_py(_build_py):
+    def run(self):
+        self.compile_shaders()
+        super().run()
+
+    def compile_shaders(self):
+        project_root = Path(__file__).parent.resolve()
+        shaders_dir = Path('chipped8', 'gui', 'shaders')
+        qsb = 'qsb'
+
+        for shader in chain(shaders_dir.glob('*.vert'), shaders_dir.glob('*.frag')):
+            output = shader.with_name(shader.name + ".qsb")
+            if not output.exists() or shader.stat().st_mtime > output.stat().st_mtime:
+                print(f'Compiling {shader.name} → {output.name}')
+                subprocess.run([
+                    qsb,
+                    '--glsl', '100 es,120,150',#'450',
+                    '--hlsl', '50',
+                    '--msl', '12',
+                    '-c',
+                    '-o', str(output),
+                    str(shader)
+                ], check=True)
+
+setup(
+    cmdclass={
+        'build_py': build_py,
+    }
+)
