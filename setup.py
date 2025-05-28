5285342d84e91d4b0a8fa4516ba2b0de27d4f4cb
John Schember <john@nachtimwald.com>
2025-05-27 21:30:53 -0400

    splitl compile shaders out into a seperate script.

--- setup.py
+++ setup.py
@@ -1,38 +1,16 @@
-import os
-import subprocess
-
-from itertools import chain
+import sys
 from pathlib import Path
-import shutil
 
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
 
+sys.path.insert(0, str(Path(__file__).resolve().parent / "tools"))
+from compile_shaders import compile_shaders
+
 class build_py(_build_py):
     def run(self):
-        self.compile_shaders()
+        compile_shaders()
         super().run()
-
-    def compile_shaders(self):
-        project_root = Path(__file__).parent.resolve()
-        shaders_dir = project_root / "chipped8" / "gui" / "shaders"
-        qsb = shutil.which('pyside6-qsb')
-        if not qsb:
-            raise RuntimeError("pyside6-qsb executable not found on PATH")
-
-        for shader in chain(shaders_dir.glob('*.vert'), shaders_dir.glob('*.frag')):
-            output = shader.with_name(shader.name + ".qsb")
-            if not output.exists() or shader.stat().st_mtime > output.stat().st_mtime:
-                print(f'Compiling {shader.name} -> {output.name}')
-                subprocess.run([
-                    qsb,
-                    '--glsl', '100 es,120,150',#'450',
-                    '--hlsl', '50',
-                    '--msl', '12',
-                    '-c',
-                    '-o', str(output),
-                    str(shader)
-                ], check=True)
 
 setup(
     cmdclass={
